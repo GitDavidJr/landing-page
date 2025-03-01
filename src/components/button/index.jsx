5079d1ec04623dac3ff3197f4140853cf19b4ca8
David Júnior <davidjunior051204@gmail.com>
2025-02-28 23:16:21 -0300

    feat: variações de botões

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,14 @@
+import { ST } from "next/dist/shared/lib/utils";
 import Styles from"./button.module.scss";  
 
-const Button =({title}) => {
-    return <button className={Styles.button}>{title}</button>
+const Button =({title, kind}) => {
+    const generationkind = () => {
+        if(kind === "secondary") {return Styles.secondary}
+        return Styles.primary
+    }
+    
+
+    return <button className={`${Styles.button} ${generationkind(kind)}`} >{title}</button>
 }
 
 export default Button;
