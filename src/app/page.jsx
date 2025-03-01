b4f149534844ce205aa97005be543e1abf299971
David Júnior <davidjunior051204@gmail.com>
2025-02-28 23:16:47 -0300

    feat: criação do componente welcome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/header';
 import "../style/globals.scss";
+import Welcome from '@/components/welcome';
 
 
 export default function Home() {
   return (
     <div className='styles.container'>
       <Header />
+      <Welcome />
     </div>
   );
 }
