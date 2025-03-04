e4eb1798896af9ebc4554236fa2011fffff6f9fb
David Júnior <davidjunior051204@gmail.com>
2025-03-04 17:48:24 -0300

    feat: adiciona componente Features e refatora estilos para responsividade

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className='styles.container'>
       <Header />
       <Welcome />
-      {/* <Features /> */}
+      <Features />
       {/* <Contato /> */}
       {/* <Footer /> */}
     </div>
