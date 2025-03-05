3991b4099db1453712b725081493b4794354e522
David Júnior <davidjunior051204@gmail.com>
2025-03-05 18:00:17 -0300

    feat: adiciona suporte ao Roboto como fonte e atualiza estilos para responsividade

--- src/app/_app.jsx
+++ src/app/_app.jsx
@@ -3,6 +3,9 @@ import App from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.css';
 
+
+
+
 class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
         let pageProps = {};
@@ -19,7 +22,10 @@ class MyApp extends App {
                 <Head>
                     <title>My Landing Page</title>
                 </Head>
-                <Component {...pageProps} />
+                <body className={roboto.variable}>
+                    <Component {...pageProps} />
+                </body>
+                
             </>
         );
     }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,16 @@
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-roboto',
+});
+
+
 export default function Layout({ children }) {
     return (
       <html>
-        <body>
+        <body className={roboto.variable}>
           {children}
         </body>
       </html>
