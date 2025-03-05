ddab5d941d953cdaa42ffe99f9f5b41129677222
David Júnior <davidjunior051204@gmail.com>
2025-03-05 16:35:56 -0300

    feat: adiciona componentes Contato e Footer, refatora estilos para responsividade

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Header />
       <Welcome />
       <Features />
-      {/* <Contato /> */}
-      {/* <Footer /> */}
+      <Contato />
+      <Footer />
     </div>
   );
 }
